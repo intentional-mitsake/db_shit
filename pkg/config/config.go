4d4a4bcd5db2f748657ae020d9af0caf7aad0a23
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-01-31 18:40:34 +0545

    destination config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,21 +3,23 @@ package config
 import "github.com/spf13/viper"
 
 type DatabaseConfig struct {
-	Host     string
-	Username string
-	Password string
-	Database string
-	Port     int
-	Type     string //mysql, pg
+	Host        string
+	Username    string
+	Password    string
+	Database    string
+	Port        int
+	Type        string //mysql, pg
+	Destination string
 }
 
 func LoadDatabaseConfig() DatabaseConfig {
 	return DatabaseConfig{
-		Host:     viper.GetString("host"),
-		Username: viper.GetString("username"),
-		Password: viper.GetString("password"),
-		Database: viper.GetString("db"),
-		Port:     viper.GetInt("port"),
-		Type:     viper.GetString("type"),
+		Host:        viper.GetString("host"),
+		Username:    viper.GetString("username"),
+		Password:    viper.GetString("password"),
+		Database:    viper.GetString("db"),
+		Port:        viper.GetInt("port"),
+		Type:        viper.GetString("type"),
+		Destination: viper.GetString("destination"),
 	}
 }
